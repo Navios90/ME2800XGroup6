ebf4e2779e6563f5321fa53c343af2af035dbd46
Navios90 <ivanb419@iastate.edu>
2024-12-12 09:45:03 -0600

    basic follow the gap

--- src/f1tenth/node/follow_the_gap.py
+++ src/f1tenth/node/follow_the_gap.py
@@ -21,13 +21,13 @@ class FollowTheGap:
         scan_topic = rospy.get_param("~scan_topic", "/scan")
 
         # Make a publisher for drive messages
-        self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=1)
+        self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
 
         # Start a subscriber to listen to odom messages
         #self.odom_sub = rospy.Subscriber(odom_topic, Odometry, self.odom_callback)
 
         # Start a subscriber to listen to laser scan messages
-        self.laser_sub = rospy.Subscriber(scan_topic, LaserScan, self.lidar_callback)
+        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.lidar_callback)
 
     #def odom_callback(self, msg):
         #pass
@@ -51,20 +51,56 @@ class FollowTheGap:
         max_gap_start = 0
         max_gap_end = 0
         max_gap_length = 0
-        
+        i = 0
+        ranges = free_space_ranges
+        while i < len(ranges):
+            if ranges[i] > 0.5:
+                gap_start = i
+                while i < len(ranges) and ranges[i] > 0.5:
+                    i += 1
+                gap_end = i - 1
+
+                gap_size = gap_end - gap_start + 1
+                if gap_size > max_gap_length:
+                    max_gap_start = gap_start
+                    max_gap_end = gap_end
+                    max_gap_length = gap_size
+
+            i += 1        
+
+        if max_gap_length > 0:
+            gap_center = (max_gap_start + max_gap_end) / 2
+            steering_angle = (gap_center - len(ranges) / 2) * np.pi / len(ranges)
+        else:
+            # No gap found, turn away from the closest obstacle
+            min_distance = min(ranges)
+            min_index = ranges.index(min_distance)
+            steering_angle = (min_index - len(ranges) / 2) * np.pi / len(ranges)
 
 
-        return max_gap_start, max_gap_end
+        return steering_angle
 
     def lidar_callback(self, data):
         """ Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
         """
         ranges = data.ranges 
         #added to handle 270 degree scan
-        speed = 1
-        steering_angle = 0
+        speed = self.set_speed(ranges)
+        steering_angle = self.find_max_gap(ranges)
+
+
         
         self.pub_drive(speed, steering_angle)
+    def set_speed(self, ranges):
+	ranges = ranges
+	if ranges[0] >5:
+              speed = 10
+	elif ranges[0] >=2:
+              speed = 2
+        else:
+              speed = 1
+
+	return speed
 
     def pub_drive(self, speed, steering_angle):
         #publish drive messages
